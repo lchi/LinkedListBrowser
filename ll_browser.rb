f4380e269d4d06e1d5495a35fa438c3bb8a50afb
Lucas C <chi.lucas@gmail.com>
2012-03-09 15:26:22 -0500

    figures out latest issue, only downloads missing entries in cache

--- ll_browser.rb
+++ ll_browser.rb
@@ -2,7 +2,9 @@ require "net/http"
 require "nokogiri"
 
 class LinkedListNYC
-  LATEST = 56
+  # gets latest from the link on the linkedlist home page
+  LATEST = Net::HTTP.get(URI("http://www.linkedlistnyc.org/")).
+    to_s.match(/archive\/issue_([\d]{3}).html/).to_s.match(/[\d]{3}/)[0].to_i
   CACHE = "linkedlist_issues"
 
   def self.method_missing(name, *args, &block)    
@@ -17,18 +19,16 @@ class LinkedListNYC
   def self.download_and_cache
     Dir.mkdir LinkedListNYC::CACHE unless Dir.exists? LinkedListNYC::CACHE
 
-    if !File.exists? "#{LinkedListNYC::CACHE}/%03d.html" % LinkedListNYC::LATEST
-      LinkedListNYC::LATEST.times do |i|
+    LinkedListNYC::LATEST.times do |i|
+      if !File.exists? "#{LinkedListNYC::CACHE}/%03d.html" % (i+1)
         File.open("#{LinkedListNYC::CACHE}/%03d.html" % (i+1), "w") do |f| 
           puts "downloading issue #{i+1}"
           f.write(Net::HTTP.get(
             URI("http://www.linkedlistnyc.org/archive/issue_%03d.html" % (i+1))))
         end
       end
-
-      return true
     end
-    return false
+    return true
   end
 end
 
